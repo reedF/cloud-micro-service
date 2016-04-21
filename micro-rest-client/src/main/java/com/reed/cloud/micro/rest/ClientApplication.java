e4701a462fd88f88aa2867e4f7a2bb00b6c9e854
reedF <reed.fym@gmail.com>
2016-04-21 15:06:35 +0800

    add zipkin for tracing rpc call

--- micro-rest-client/src/main/java/com/reed/cloud/micro/rest/ClientApplication.java
+++ micro-rest-client/src/main/java/com/reed/cloud/micro/rest/ClientApplication.java
@@ -6,12 +6,13 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
+import com.reed.cloud.micro.eureka.AbstractApplication;
+
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients(basePackages = "com.reed.cloud.micro.service")
 @EnableHystrix
-// @ComponentScan("com.reed.cloud.micro.rest.controller,com.reed.cloud.micro.service")
-public class ClientApplication {
+public class ClientApplication extends AbstractApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientApplication.class, args);
