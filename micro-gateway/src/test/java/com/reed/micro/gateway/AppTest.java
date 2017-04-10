2fb34af5fcb6eb2d6bd2481dc1bb137938d6ed27
reedF <reed.fym@gmail.com>
2017-04-10 11:00:08 +0800

    add test case

--- micro-gateway/src/test/java/com/reed/micro/gateway/AppTest.java
+++ micro-gateway/src/test/java/com/reed/micro/gateway/AppTest.java
@@ -1,38 +1,56 @@
 package com.reed.micro.gateway;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.ContentResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ZuulServerApplication.class)
+@WebAppConfiguration
+@IntegrationTest("server.port:0")
+public class AppTest {
+	private final static String url = "http://localhost:8761";
+	@Autowired
+	private WebApplicationContext wac;
+	private MockMvc mvc;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@Before
+	public void setUp() {
+		mvc = MockMvcBuilders.webAppContextSetup(wac).build();
+	}
+
+	@Test
+	public void testHealth() throws Exception {
+		ResultActions actions = mvc.perform(MockMvcRequestBuilders.post(
+				"/health").accept(MediaType.APPLICATION_JSON_UTF8));
+		ContentResultMatchers c = content();
+		actions.andExpect(status().isOk());
+		String r = actions.andReturn().getResponse().getContentAsString();
+		System.out.println(r);
+	}
+
+	@Test
+	public void testRoutes() throws Exception {
+		ResultActions actions = mvc.perform(MockMvcRequestBuilders.post(
+				"/routes").accept(MediaType.APPLICATION_JSON_UTF8));
+		ContentResultMatchers c = content();
+		actions.andExpect(status().isOk());
+		String r = actions.andReturn().getResponse().getContentAsString();
+		System.out.println(r);
+	}
 }
